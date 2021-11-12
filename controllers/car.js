cfc7fc1d46a4789e2bb95524a28d6c55bf111fc6
praneethkumar695 <praneethkumar695@gmail.com>
2021-11-12 00:13:23 -0600

    Updated few files

--- controllers/car.js
+++ controllers/car.js
@@ -4,9 +4,16 @@ exports.car_list = function (req, res) {
     res.send('NOT IMPLEMENTED: Car list');
 };
 // for a specific Car.
-exports.car_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: Car detail: ' + req.params.id);
-};
+exports.car_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Car.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
 // Handle Car create on POST.
 exports.car_create_post = async function (req, res) {
     console.log(req.body)
@@ -30,10 +37,25 @@ exports.car_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: Car delete DELETE ' + req.params.id);
 };
 // Handle Car update form on PUT.
-exports.car_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: Car update PUT' + req.params.id);
+exports.car_update_put = async function(req, res) {
+ console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+ try {
+ let toUpdate = await Car.findById( req.params.id)
+ // Do updates of properties
+ if(req.body.carName)
+ toUpdate.carName = req.body.carName;
+ if(req.body.carType) toUpdate.carType = req.body.carType;
+ if(req.body.carCost) toUpdate.carCost = req.body.carCost;
+ let result = await toUpdate.save();
+ console.log("Sucess " + result)
+ res.send(result)
+ } catch (err) {
+ res.status(500)
+ res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+ }
 };
-
 // List of all Cars
 exports.car_list = async function (req, res) {
     try {
